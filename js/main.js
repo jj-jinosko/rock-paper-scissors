c6116163b6a71091120bfdb27abea5766875489b
jj-jinosko <jesscj@umich.edu>
2023-11-24 17:49:48 -0500

    add user input and test options to check moves

--- js/main.js
+++ js/main.js
@@ -2,13 +2,14 @@ console.log("Test")
 
 // ---------- Player Input: --------------
 
+let moves = ['rock', 'paper', 'scissors'];
 
 // Computer player input (random selection)
 
-function getComputerChoice() {
-    let moves = ['rock', 'paper', 'scissors']; 
-    let rand = moves[(Math.random() * moves.length) | 0]
-    console.log(rand)
+function getComputerChoice() { 
+    let rand = moves[(Math.random() * moves.length) | 0];
+    console.log(rand); // for testing purposes
+    return rand;
 }
 
 // User input from prompt 
@@ -16,10 +17,44 @@ function getComputerChoice() {
 //     - valid input = cont.
 //     - invald input = retry
 
-// Game Status:
+function getUserChoice() {
+    let userChoice = prompt('What item do you choose?');
+
+    // could use a try/catch here
+
+    // if userChoice == invalid {
+    
+    if (userChoice.toLowerCase() == 'rock') {
+        console.log('this worked')
+    }
+
+    // }
+
+    // seems like "in" works differently than python
+    // if (userChoice.toLowerCase() in moves) {
+    //     console.log('nice')
+    //     return userChoice.toLowerCase()
+    // }
+
+    if (moves.includes(userChoice.toLowerCase())) {
+        console.log('nice')
+    }
+
+}
+
+// ---------- Game Status: ---------------
 // - compare player moves
 //     - win
 //     - tie
 
 
+// ---------- Player Input: --------------
+// Computer player input (random selection)
 getComputerChoice()
+// User input from prompt 
+getUserChoice()
+
+// ---------- Game Status: ---------------
+// - compare player moves
+//     - win
+//     - tie
