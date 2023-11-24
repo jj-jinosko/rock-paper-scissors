36fda2670b3450779859e0fa150ebc64d819c7b0
jj-jinosko <jesscj@umich.edu>
2023-11-24 17:21:45 -0500

    add computer player input and game outline to js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
 console.log("Test")
+
+// ---------- Player Input: --------------
+
+
+// Computer player input (random selection)
+
+function getComputerChoice() {
+    let moves = ['rock', 'paper', 'scissors']; 
+    let rand = moves[(Math.random() * moves.length) | 0]
+    console.log(rand)
+}
+
+// User input from prompt 
+//     - no input = quit
+//     - valid input = cont.
+//     - invald input = retry
+
+// Game Status:
+// - compare player moves
+//     - win
+//     - tie
+
+
+getComputerChoice()
